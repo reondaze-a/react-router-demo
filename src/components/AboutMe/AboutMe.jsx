e9fd8ee4beee1bf9cb91ae960ff4d29ed0d0207a
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-05-21 01:30:29 -0400

    Added more stuff
    Added different codes to test out the new elements

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,9 +1,26 @@
+import './AboutMe.css';
+import { Outlet, Link } from 'react-router-dom'; // New import
+
 function AboutMe() {
+  // Add the classNames shown below so the styles are applied.
+  // Add the links as shown as shown below.
   return (
-    <div>
+    <div className="about">
+      <ul className="links">
+        <li>
+          <Link to="my-story">My Story</Link>
+        </li>
+        <li>
+          <Link to="hobbies">Hobbies</Link>
+        </li>
+        <li>
+          <Link to="contact">My Contact Info</Link>
+        </li>
+      </ul>
       <p>I&apos;m a simple person. I see Emojis, I write reviews.</p>
+      <Outlet />
     </div>
-  );
+  )
 }
 
 export default AboutMe;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import AboutUs from "../AboutUs/AboutUs";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
