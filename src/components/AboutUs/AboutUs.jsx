e36f46c151a79808ee255122060113e1796f47aa
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-05-21 10:59:24 -0400

    added reviews
    added reviews section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
 import "./AboutUs.css";
+import { Outlet, Link } from "react-router-dom";
 
 function AboutUs() {
   return (
     <div className="about-us">
       <p>You can find out more information about our site here.</p>
+      <ul>
+        <li>
+          <Link to="site-history">Site History</Link>
+        </li>
+        <li>
+          <Link to="site-mission">Site Mission</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
   );
 }
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,22 +7,43 @@ import AboutUs from "../AboutUs/AboutUs";
 import Contact from "../AboutMe/Contact";
 import Hobbies from "../AboutMe/Hobbies";
 import MyStory from "../AboutMe/MyStory";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.nomoreparties.co/emoji-critic-ens")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .then((data) => {
+        setReviews(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews}/>} />
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
           <Route path="hobbies" element={<Hobbies />} />
           <Route path="my-story" element={<MyStory />} />
         </Route>
-
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="site-history" element={<SiteHistory />} />
+          <Route path="site-mission" element={<SiteMission />} />
+        </Route>
       </Routes>
     </div>
   );
